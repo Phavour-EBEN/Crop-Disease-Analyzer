9e3c93882cf92083c153b90f28662b8588467959
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-04-05 13:27:58 +0000

    add a flask app

--- cropApp.py
+++ cropApp.py
@@ -78,4 +78,3 @@ if __name__ == '__main__':
     # For development only - use proper WSGI server in production
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     
-    
